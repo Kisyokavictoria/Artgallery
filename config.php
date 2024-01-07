f67a56269fb400119a58c345f5b36f888bb142a6
Kisyoka7 <kisyokav@gmail.com>
2024-01-07 09:35:03 +0300

    second commit

--- config.php
+++ config.php
@@ -1,5 +1,24 @@
 <?php
+//database connection details
 session_start();
-$conn = mysqli_connect("localhost", "root", "gallery_syst")
+
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'gallery_syst';
+
+//creating the connection
+//when passing the arguements they should follow that order
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if($conn){
+    echo "connected successfully!";
+
+}else{
+    die("connection failed: " . mysqli_connect_error());
+}
+
+// $conn->close();
+
 
 ?>
